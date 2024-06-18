94985fef055d2f28f604325aa2f4751d8a51b4ab
Anita Yawson <anitayawson@gmail.com>
2024-06-18 00:02:46 -0400

    set background images for all media breakpoints

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -2,11 +2,5 @@ import { ButtonOne, ButtonTwo } from "../../components/Buttons/Buttons";
 import "./Home.scss";
 
 export default function Home() {
-  return (
-    <div>
-      {/* <h1>Home</h1> */}
-      <ButtonOne />
-      <ButtonTwo />
-    </div>
-  );
+  return <div className="home"></div>;
 }
