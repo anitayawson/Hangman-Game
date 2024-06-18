740aa53092280504667d30bf660eb989b1dfeec4
Anita Yawson <anitayawson@gmail.com>
2024-06-18 01:25:21 -0400

    add styling for Home page at all breakpoints

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,18 @@
 import { ButtonOne, ButtonTwo } from "../../components/Buttons/Buttons";
 import "./Home.scss";
+import logo from "../../assets/images/logo.svg";
+import PlayButton from "../../assets/images/icon-play.svg";
 
 export default function Home() {
-  return <div className="home"></div>;
+  return (
+    <div className="home">
+      <article className="home__content">
+        <img src={logo} alt="Game Logo" className="logo" />
+        <button className="home__play-btn">
+          <img className="home__play-icon" src={PlayButton} alt="Play" />
+        </button>
+        <button className="home__instructions-btn">How to Play</button>
+      </article>
+    </div>
+  );
 }
