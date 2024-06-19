9a59ccd3f07b756cc81005ec2e988a64b4644013
Anita Yawson <anitayawson@gmail.com>
2024-06-18 23:24:11 -0400

    add route to instructions page, refactor page styling to make background img reusable on all pages

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import "./Home.scss";
 import logo from "../../assets/images/logo.svg";
 import PlayButton from "../../assets/images/icon-play.svg";
+import { Link } from "react-router-dom";
 
 export default function Home() {
   return (
@@ -10,7 +11,9 @@ export default function Home() {
         <button className="home__play-btn">
           <img className="home__play-icon" src={PlayButton} alt="Play" />
         </button>
-        <button className="home__instructions-btn">How to Play</button>
+        <Link to="/how-to-play" className="home__instructions-btn">
+          How to Play
+        </Link>
       </article>
     </div>
   );
