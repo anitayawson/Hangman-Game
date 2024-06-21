2b4b87a029b21e8b66e72cf259ac4d6c11ab6771
Anita Yawson <anitayawson@gmail.com>
2024-06-20 22:19:56 -0400

    add elements & styling to categories page, refactor page styles to use new mixins

--- src/pages/Categories/Categories.js
+++ src/pages/Categories/Categories.js
@@ -1,6 +1,33 @@
 import "./Categories.scss";
-import "../../data/data.json";
+import BackIcon from "../../assets/images/icon-back.svg";
+import useNavigation from "../../hooks/useNavigation";
+import categoriesData from "../../data/data.json";
 
-export default function Home() {
-  return <div>Pick a Category</div>;
+export default function Categories() {
+  const { navigateBack } = useNavigation();
+
+  const categories = categoriesData.categories;
+
+  const categoryNames = [];
+  for (const category in categories) {
+    categoryNames.push(category);
+  }
+
+  return (
+    <div className="categories">
+      <nav className="categories__nav">
+        <button onClick={navigateBack} className="back-btn">
+          <img className="back-btn__icon" src={BackIcon} alt="Back Icon" />
+        </button>
+        <div className="categories__title"></div>
+      </nav>
+      <ul className="categories__list">
+        {categoryNames.map((categoryName, index) => (
+          <li key={index} className="categories__list-item">
+            {categoryName}
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
