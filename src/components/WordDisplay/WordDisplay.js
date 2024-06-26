331c49675cea8a6857c17cbc8277ec1442c0fd77
Anita Yawson <anitayawson@gmail.com>
2024-06-25 20:03:54 -0400

    add disabled state to display letters

--- src/components/WordDisplay/WordDisplay.js
+++ src/components/WordDisplay/WordDisplay.js
@@ -6,7 +6,14 @@ export default function WordDisplay({ word, guessedLetters }) {
       {word.split(" ").map((word, wordIndex) => (
         <div key={wordIndex} className="word-display__word">
           {word.split("").map((letter, letterIndex) => (
-            <div key={letterIndex} className="word-display__letter">
+            <div
+              key={letterIndex}
+              className={`word-display__letter ${
+                guessedLetters.includes(letter.toUpperCase())
+                  ? ""
+                  : "word-display__letter--disabled"
+              }`}
+            >
               {guessedLetters.includes(letter.toUpperCase()) ? letter : " "}
             </div>
           ))}
